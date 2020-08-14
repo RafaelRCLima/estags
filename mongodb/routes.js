25f414f59ef5501d408007d63968e1d2871e4f68
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-14 10:38:54 -0300

    Examples for sort and unwind

--- mongodb/routes.js
+++ mongodb/routes.js
@@ -26,7 +26,34 @@ router.get('/match', async (req, res) => {
 })
 
 router.get('/sort', async (req, res) => {
-  req.json({ sort: true })
+  const sort = await movie.aggregate([{ $match: { "type": "movie" }},
+  { $project: { "title": 1, "year": 1, "type": 1 } },
+  { $limit: 10 },
+  { $sort: { "year": 1 }}
+])
+  res.json(sort)
+})
+
+router.get('/skip', async (req, res) => {
+  let { page = 1 } = req.query
+  const toSkip = (page - 1) * 10 
+  const skip = await movie.aggregate([
+    { $match: { "runtime": 88 } },
+    { $project: { "title": 1, "runtime": 1 } },
+    { $skip: toSkip },
+    { $limit: 10 }
+  ])
+
+  res.json(skip)
+})
+
+router.get('/unwind', async (req, res) => {
+  const unwind = await movie.aggregate([
+    { $project: { "genres": 1 } },
+    { $unwind: "$genres" },
+    { $limit: 10 }
+  ])
+  res.json(unwind)
 })
 
 module.exports = router
