304adb7d00cca3cc37199fafd19898e92a8cdf8e
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-14 00:48:47 -0300

    Aggregation Project example implemmented

--- mongodb/app.js
+++ mongodb/app.js
@@ -1,25 +1,14 @@
 const express = require('express')
-const { Router } = require('express')
 
 const movie = require('./movie')
+const routes = require('./routes')
 
 const app = express()
 app.use(express.json())
 
-const router = new Router()
-
 require('./config')
 
-router.get('/home', async (req, res) => {
-  const movieFound = await movie.findOne()
-  res.json(movieFound)
-})
-
-router.get('/', (req, res) => {
-  res.json('It works')
-})
-
-app.use(router)
+app.use(routes)
 
 app.listen(3000, function(){
   console.log('Rodando na 3000')
---
+++ mongodb/routes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const router = new Router()
+
+const movie = require('./movie')
+
+router.get('/home', async (req, res) => {
+  const movieFound = await movie.findOne()
+  res.json(movieFound)
+})
+
+router.get('/', (req, res) => {
+  res.json('It works')
+})
+
+router.get('/project', async (req, res) => {
+  const project = await movie.aggregate([{ $project: { "plot": 2 } }]).limit(10)
+  res.json(project)
+})
+
+module.exports = router
