abd5957497908082bda4fa1a865e8fa59bda5deb
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-17 15:15:56 -0300

    ElasticSearch examples

---
+++ elasticsearch/exampleSearch.js
@@ -0,0 +1,21 @@
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({ node: 'http://localhost:9200' })
+
+async function buscar () {
+  await client.indices.refresh({ index: 'game-of-thrones' })
+
+  // Let's search!
+  const { body } = await client.search({
+    index: 'game-of-thrones',
+    // type: '_doc', // uncomment this line if you are using {es} ≤ 6
+    body: {
+      query: {
+        match: { character: 'Tyrion' }
+      }
+    }
+  })
+
+  console.log(body.hits.hits)
+}
+
+buscar().catch(console.log)
