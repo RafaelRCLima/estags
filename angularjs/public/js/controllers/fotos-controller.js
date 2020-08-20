bb378cd1fbe133382cbc1de62e98830e154161b9
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-20 13:46:18 -0300

    Angular config

---
+++ angularjs/public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('node_estags').controller('FotosController', function($scope) {
+
+  $scope.foto = {
+    titulo: 'Gabriel',
+    url: 'https://avatars3.githubusercontent.com/u/16672448?v=4'
+  }
+})
