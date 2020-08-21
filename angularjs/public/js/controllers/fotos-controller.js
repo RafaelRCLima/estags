f8a028b3202eba1350f839680b2353ffb280395b
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-21 14:10:22 -0300

    Accessing data from backend using

--- angularjs/public/js/controllers/fotos-controller.js
+++ angularjs/public/js/controllers/fotos-controller.js
@@ -1,17 +1,12 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-  $scope.fotos = [
-    {
-      titulo: 'Gabriel',
-      url: 'https://avatars3.githubusercontent.com/u/16672448?v=4'
-    },
-    { 
-      titulo: 'Matheus',
-      url: 'https://avatars2.githubusercontent.com/u/42144731?v=4'  
-    },
-    {
-      titulo: 'Rafael',
-      url: 'https://avatars1.githubusercontent.com/u/38574242?v=4'
-    }
-]
+  $scope.fotos = []
+
+  $http.get('v1/fotos')
+  .success(function(fotos) {
+    $scope.fotos = fotos
+  })
+  .error(function(erro) {
+    console.log(erro)
+  })
 })
