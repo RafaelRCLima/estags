2cc0cd427470fb1a637c2c5ad9e469c90db3add0
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-21 15:18:33 -0300

    Transclusion and personal directive

---
+++ angularjs/public/js/directives/minhas-diretivas.js
@@ -0,0 +1,23 @@
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', function() {
+  var ddo = {}
+
+  ddo.scope = {
+    titulo: '@'
+  }
+
+  ddo.transclude = true
+
+  ddo.template = 
+      '<div class="panel panel-default">'
+    + ' <div class="panel-heading">'
+    + '   <h3 class="panel-title text-center">{{titulo}}</h3>'
+    + ' </div>'
+    + ' <div class="panel-body" ng-transclude>'
+    + ' </div>'
+    + '</div>'
+
+  ddo.restric = "AE"
+  return ddo
+})
+
