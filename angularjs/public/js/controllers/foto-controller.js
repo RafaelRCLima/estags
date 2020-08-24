093d54df5e279cb7486c105b7e67721ad6bc2258
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-24 16:12:54 -0300

    Edit and remove added

--- angularjs/public/js/controllers/foto-controller.js
+++ angularjs/public/js/controllers/foto-controller.js
@@ -1,17 +1,38 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http) {
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
 
-  $scope.foto = {}
+  $scope.foto = {},
+  $scope.mensagem = ''
 
-  $scope.submeter = function() {
-    if ($scope.formulario.$valid) {
-      $http.post('v1/fotos', $scope.foto)
-    .success(function() {
-      $scope.foto = {}
-      console.log('Foto cadastrada')
+  if ($routeParams.fotoId) {
+    $http.get('v1/fotos/' + $routeParams.fotoId)
+    .success(function(foto) {
+      $scope.foto = foto
     })
     .error(function(erro) {
       console.log(erro)
+      $scope.mensagem = "Não foi possível obter a foto"
     })
+  }
+
+  $scope.submeter = function() {
+    if ($scope.formulario.$valid) {
+      if($scope.foto._id) {
+        $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+        .success(function() {
+          $scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada com sucesso.'
+        })
+      } else {
+        $http.post('v1/fotos', $scope.foto)
+        .success(function() {
+          $scope.foto = {}
+          $scope.mensagem = 'Foto incluída'
+        })
+        .error(function(erro) {
+          $scope.mensagem = 'Erro ao cadastrar a foto'
+          console.log(erro)
+        })
+        }
+      
     }
   }
 
--- angularjs/public/js/controllers/fotos-controller.js
+++ angularjs/public/js/controllers/fotos-controller.js
@@ -1,5 +1,5 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
-
+  
   $scope.fotos = []
   $scope.filtro = ''
 
@@ -10,4 +10,18 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
   .error(function(erro) {
     console.log(erro)
   })
+
+  $scope.remover = function(foto) {
+    $http.delete('v1/fotos/' + foto._id)
+    .success(function() {
+      var indiceFoto = $scope.fotos.indexOf(foto)
+      $scope.fotos.splice(indiceFoto, 1)
+      $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso'
+    })
+    .error(function(erro) {
+      console.log(erro)
+      $scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo
+    })
+  }
+
 })
