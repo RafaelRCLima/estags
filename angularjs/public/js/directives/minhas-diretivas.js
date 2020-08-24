28c0794039a7f63159e0faa79f130364b7fcf6b3
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-24 17:16:02 -0300

    Added form group

--- angularjs/public/js/directives/minhas-diretivas.js
+++ angularjs/public/js/directives/minhas-diretivas.js
@@ -11,6 +11,35 @@ angular.module('minhasDiretivas', [])
   ddo.templateUrl = 'js/directives/meu-painel.html'
 
   ddo.restric = "AE"
+
   return ddo
 })
+.directive('minhaFoto', function() {
+  
+  var ddo = {}
 
+  ddo.restric = "AE"
+
+  ddo.scope = {
+    titulo: '@',
+    url: '@'
+  }
+
+  ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">'
+
+  return ddo
+})
+.directive('meuBotaoPerigo', function() {
+
+  var ddo = {}
+  ddo.restric = "E"
+
+  ddo.scope = {
+    nome: '@',
+    acao: '&'
+  }
+
+  ddo.template = '<button ng-click="acao(foto)" class="btn btn-danger btn-block">{{nome}}</button>'
+
+  return ddo
+})
