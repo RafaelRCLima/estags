e5c90b424ee90ff42c218e0e9e4d1bc1a5dab452
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-08-25 15:45:47 -0300

    Focusing 'voltar' button after a saving operation

--- angularjs/public/js/directives/minhas-diretivas.js
+++ angularjs/public/js/directives/minhas-diretivas.js
@@ -43,3 +43,17 @@ angular.module('minhasDiretivas', [])
 
   return ddo
 })
+.directive('meuFocus', function() {
+  
+  var ddo = {}
+
+  ddo.restric = "A"
+  
+  ddo.link = function(scope, element) {
+    scope.$on('fotoCadastrada', function() {
+      element[0].focus()
+    })
+  }
+
+  return ddo
+})
